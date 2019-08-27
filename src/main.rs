423cf88ba914f3a62e77017440cb934166d69351
kmkzt <info.pscreator@gmail.com>
2019-08-28 00:29:15 +0900

    feat: add image_mock routing

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,33 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::request::Form;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+/// image_mock 
+#[derive(FromForm)]
+struct ImageInfo {
+    width: u8,
+    height: u8
+}
+
+#[get("/image?<format>&<info..>")]
+fn image_mock(format: Option<String>, info: Option<Form<ImageInfo>>) -> String {
+    let image_format = match format {
+        Some(ext) => format!("image is {}!", ext),
+        None => "default is png.".to_string()
+    };
+
+    let image_size = match info {
+        Some(_form) => "size is changed!".to_string(),
+        None => "100x100 is default size".to_string()
+    };
+
+    format!("{} {}", image_format, image_size)
 }
 
 fn main() {
